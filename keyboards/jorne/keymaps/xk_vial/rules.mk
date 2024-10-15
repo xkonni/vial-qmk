58a8c3a9473e17f5b472232380ff273f63ec6cd9
Konstantin Koslowski <konstantin.koslowski@posteo.de>
2024-10-15 21:44:12 +0200

    jorne: add keymap xk_vial

---
+++ keyboards/jorne/keymaps/xk_vial/rules.mk
@@ -0,0 +1,18 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes		# compile optimization, reduce size
+
+# Can't have RGBLIGHT and RGB_MATRIX at the same time.
+RGBLIGHT_ENABLE     = yes
+RGB_MATRIX_ENABLE   = no
+
+OLED_ENABLE         = yes
+OLED_DRIVER 		= ssd1306
+COMBO_ENABLE        = no
+MOUSEKEY_ENABLE 	= no      	# Mouse keys
+EXTRAKEY_ENABLE 	= yes     	# Audio control and System control
+SPACE_CADET_ENABLE  = yes	    # ( on tap L_SFT, ) on tap R_SFT
+
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE 	= no    	# Breathing sleep LED during USB suspend
+QMK_SETTINGS        = no
