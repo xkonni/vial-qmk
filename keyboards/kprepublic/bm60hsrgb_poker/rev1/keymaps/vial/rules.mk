01a370db0396afdff2ed124f253b438e4c1edb40
Konstantin Koslowski <konstantin.koslowski@posteo.de>
2025-08-13 23:20:01 +0200

    bm60hsrgb_poker/rev1 fix vial keymap

--- keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/vial/rules.mk
+++ keyboards/kprepublic/bm60hsrgb_poker/rev1/keymaps/vial/rules.mk
@@ -10,5 +10,9 @@ TAP_DANCE_ENABLE = yes
 QMK_SETTINGS = no
 KEY_OVERRIDE_ENABLE = yes
 
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 VIALRGB_ENABLE = yes        # Enable Vial GUI
+NKRO_ENABLE = yes
+
+EXTRAKEY_ENABLE = yes
+MAGIC_ENABLE = yes
